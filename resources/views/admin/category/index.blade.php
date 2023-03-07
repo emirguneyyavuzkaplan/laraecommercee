e095c7cc1322f5a235f013fda8ba4228478582e4
emirguneyyavuzkaplan <emirguneyyavuzkaplan@gmail.com>
2023-03-08 01:08:49 +0300

    Initial commit

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12 ">
+            <div class="row">
+                <div class="col-md-12">
+                    @if(session('message'))
+                        <h6 class="alert alert-success">{{session('message')}}</h6>
+                    @endif
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Category
+                            <a href="{{url('admin/category/create')}}" class="btn btn-primary btn-sm float-end">Add Category</a>
+                            </h4>
+                        </div>
+                        <div class="card-body">
+
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
